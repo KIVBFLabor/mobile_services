915c1647a0281ee671c836822d9bd530587a57c5
WÃ¤chter Sascha <sascha.waechter@kivbf.de>
2016-06-25 10:03:16 +0000

    Layout angepasst -> Funktionalität folgt (Sascha)

--- netflix_randomizer/webapp/controller/View1.controller.js
+++ netflix_randomizer/webapp/controller/View1.controller.js
@@ -5,20 +5,38 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	*@memberOf netflix_randomizer.controller.View1
 	*/
 search_actor: function () {
-			var actor = this.getView().byId("__input0").getValue();
 			
+		},
+		/**
+	*@memberOf netflix_randomizer.controller.View1
+	*/
+on_search: function () {
+	
+		  /*
+			var actor = this.getView().byId("__input0").getValue();
+			var req = new XMLHttpRequest();
+			var URL = "https://netflixroulette.net/api/api.php?";
+			var queryType = "actor=";
+			var query = actor;
+			console.log(actor);
+			req.open("GET", URL + queryType + query, true);
+			req.addEventListener("load", function () {
+				var response = JSON.parse(req.responseText);
+				console.log(response);
+			});
+			//this.getView().byId("__input5").setValue(response.director);
+			req.send(null);
+			*/
 			var req = new XMLHttpRequest();
 
 var URL = "https://netflixroulette.net/api/api.php?";
 var queryType = "actor=";
-var query = actor;
+var query = "statham";
 
 req.open('GET', URL + queryType + query, true);
 req.addEventListener("load", function() {
 	var response = JSON.parse(req.responseText);
-	response.forEach(function(elem){
-  	console.log(elem.show_title);
-  });
+	console.log(response);
 });
 req.send(null);
 		}
