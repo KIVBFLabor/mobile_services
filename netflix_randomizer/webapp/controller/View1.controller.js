a4f07581ead8c8f137a1f4fee0518b750c77cd7e
WÃ¤chter Sascha <sascha.waechter@kivbf.de>
2016-06-25 18:24:23 +0000

    Funktionelle Logik eingebaut -> muss nur noch auf die Oberfläche gebracht werden

--- netflix_randomizer/webapp/controller/View1.controller.js
+++ netflix_randomizer/webapp/controller/View1.controller.js
@@ -4,41 +4,42 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 		/**
 	*@memberOf netflix_randomizer.controller.View1
 	*/
-search_actor: function () {
-			
-		},
-		/**
-	*@memberOf netflix_randomizer.controller.View1
-	*/
 on_search: function () {
 	
-		  /*
-			var actor = this.getView().byId("__input0").getValue();
-			var req = new XMLHttpRequest();
-			var URL = "https://netflixroulette.net/api/api.php?";
-			var queryType = "actor=";
-			var query = actor;
-			console.log(actor);
-			req.open("GET", URL + queryType + query, true);
-			req.addEventListener("load", function () {
-				var response = JSON.parse(req.responseText);
-				console.log(response);
-			});
-			//this.getView().byId("__input5").setValue(response.director);
-			req.send(null);
-			*/
-			var req = new XMLHttpRequest();
+		var cast;
+	
+		var req = new XMLHttpRequest();
 
-var URL = "https://netflixroulette.net/api/api.php?";
-var queryType = "actor=";
-var query = "statham";
+		var URL = "https://netflixroulette.net/api/api.php?";
+		var queryType = "actor=";
+		var query = "benedict+cumberbatch";
 
-req.open('GET', URL + queryType + query, true);
-req.addEventListener("load", function() {
-	var response = JSON.parse(req.responseText);
-	console.log(response);
-});
-req.send(null);
-		}
+
+		var queryString = "type=json";
+		            queryString += "&actor=statham";
+		            queryString += "&year=2015";
+		 
+		
+		
+		
+		req.open("GET", URL + queryString.replace(/\s/ig, "%20"), true);
+		req.onreadystatechange = function() {
+		  if (req.readyState !== 4) {
+		                return;
+		            }
+		
+		            if (req.status !== 200) {
+		                throw new Error("Unexpected HTTP Status Code (" + req.status + ")");
+		            }
+		  
+			var response = JSON.parse(req.responseText);
+			cast = response[0].show_cast;
+			
+		};
+		req.send();
+		
+		//this.getView().byID("__area1").setValue(cast);
+		alert(cast);
+}
 	});
 });
