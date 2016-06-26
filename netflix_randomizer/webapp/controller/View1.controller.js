586d1b6faa1a40887d9e249d6ae0c4a74c6e32c3
WÃ¤chter Sascha <sascha.waechter@kivbf.de>
2016-06-26 11:52:26 +0000

    Erste funktionierende Version! -> Suche nach Schauspieler funtiioniert

--- netflix_randomizer/webapp/controller/View1.controller.js
+++ netflix_randomizer/webapp/controller/View1.controller.js
@@ -4,42 +4,60 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 		/**
 	*@memberOf netflix_randomizer.controller.View1
 	*/
-on_search: function () {
 	
+on_search: function () {
+
 		var cast;
+		var title;
+		var director;
+		var summary;
+		var poster;
+		var rating;
+		
+		var count;
+		
+				var apple = new function() {
+    this.type = "macintosh";
+    this.color = "red";
+    this.getInfo = function () {
+        return this.color + ' ' + this.type + ' apple';
+    };
+}
 	
 		var req = new XMLHttpRequest();
 
 		var URL = "https://netflixroulette.net/api/api.php?";
-		var queryType = "actor=";
-		var query = "benedict+cumberbatch";
-
 
 		var queryString = "type=json";
-		            queryString += "&actor=statham";
-		            queryString += "&year=2015";
+				if(this.getView().byId("__input0").getValue() !== ""){
+					queryString += "&actor=" + this.getView().byId("__input0").getValue();
+				}
+		    
 		 
+		req.open("GET", URL + queryString.replace(/\s/ig, "%20"), false);
+		req.send(null);
 		
-		
-		
-		req.open("GET", URL + queryString.replace(/\s/ig, "%20"), true);
-		req.onreadystatechange = function() {
-		  if (req.readyState !== 4) {
-		                return;
-		            }
-		
-		            if (req.status !== 200) {
-		                throw new Error("Unexpected HTTP Status Code (" + req.status + ")");
-		            }
-		  
+		if(req.status === 200){
+			count = 0;
 			var response = JSON.parse(req.responseText);
-			cast = response[0].show_cast;
-			
-		};
-		req.send();
+		 
+		 	cast = response[count].show_cast;
+		 	this.getView().byId("__area1").setValue("Schauspieler: " + cast);
+		 	
+		 	title = response[count].show_title;
+		 	this.getView().byId("__input3").setValue("Titel: " + title);
+		 	
+		 	director = response[count].director;
+		 	this.getView().byId("__input5").setValue("Regisseur: " + director);
+		 	
+		 	summary = response[count].summary;
+		 	this.getView().byId("__area0").setValue("Handlung: " + summary);
+		 	
+		 	poster = response[count].poster;
+		 	this.getView().byId("__image0").setSrc(poster);
+		}
 		
-		//this.getView().byID("__area1").setValue(cast);
-		alert(cast);
-}
+	}
+	
 	});
 });
